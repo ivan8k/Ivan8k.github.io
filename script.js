c1e223573884a256e57071852e49cc781281a54c
ivan8k <48686648+ivan8k@users.noreply.github.com>
2019-10-20 23:01:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,10 @@
-function makePreviewBox(title, image, desc)
-{
-	
-	var ret = '<div class="previewbox" id="preview' + n + '"><p class="title"><b>' + title +
-	'</b></p><img class="image" alt="" src="' + image + '"></img><p class="desc">' + desc + '</p></div>';
-	n += 1;
-	return ret;
-}
-
-function link_preview(link)
-{
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function()
-	{
-		if (this.readyState == 4)
-		{
-			var data = JSON.parse(this.responseText);
-			if(data.title == "")
-			{
-				alert("Erro");
-				return;
-			}
-			document.getElementById("mainbox").innerHTML = makePreviewBox(data.title, data.image, data.description) +
-			document.getElementById("mainbox").innerHTML;
-			/*document.getElementById("title").innerHTML = "<b>" + data["title"] + "</b>";
-			document.getElementById("image").src = data["image"];
-			document.getElementById("desc").innerHTML = data["description"];*/
-		}
-	};
-	xhttp.open("GET", "https://api.linkpreview.net?key=5c9bb9554b13d41f1a4f7e9c9800871a1632a2dea5d06&q=" + link, true);
-	//xhttp.setRequestHeader("Access-Control-Allow-Origin", "");
-	xhttp.send();
-	//return pos;
-}
-
-function buttonClick()
-{
-	link_preview(document.getElementById("input").value);
-}
-
-var n = 1;
-//link_preview("https://www.google.com");
+$("#btn").click(function(){
+  $("#btn").hide();
+  $("#img-normal").show();
+  $("body").css("background-color", "black");
+  setInterval(function(){
+    $("#img-normal").toggle();
+    $("#img-negativa").toggle();
+  }, 10);
+  $("audio")[0].play();
+})
